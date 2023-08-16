8db25c3efb2a29468edf751ae1cab10bbcff8e6a
TheRagnar18K <101288835+theragnarokpv@users.noreply.github.com>
2023-08-15 23:37:57 -0600

    Add files via upload

--- src/main/resources/DB_KADA.sql
+++ src/main/resources/DB_KADA.sql
@@ -6,7 +6,7 @@ CREATE SCHEMA dbkada;
 /*Se crea un usuario para la base de datos llamado "usuario_prueba" y tiene la contraseña "Usuario_Clave."*/
 create user 'usuario_admin'@'%' identified by 'Clave_admin';
 
-/*Se asignan los prvilegios sobr ela base de datos Ideas Kada al usuario creado */
+/*Se asignan los prvilegios sobr ela base de datos TechShop al usuario creado */
 grant all privileges on dbkada.* to 'usuario_admin'@'%';
 flush privileges;
 
@@ -67,6 +67,30 @@ create table dbkada.rol (
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4;
 
+create table dbkada.tipo (
+  id_tipo INT NOT NULL AUTO_INCREMENT,
+  descripcion varchar(20),
+  activo bool,
+  PRIMARY KEY (id_tipo)
+)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4;
+
+create table dbkada.pedido (
+  id_pedido INT NOT NULL AUTO_INCREMENT,
+  nombre varchar(20),
+  pedido INT NOT NULL,
+  descripcion varchar(1000),
+  telefono varchar(10),
+  correo_elec varchar(100),
+  ruta_imagen varchar(1024),
+  activo boolean,
+  PRIMARY KEY (id_pedido),
+  foreign key fk_pedido_tipo (pedido) references tipo(id_tipo)
+)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4;
+
 INSERT INTO dbkada.usuario (id_usuario, username,password,nombre, apellidos, correo, telefono,ruta_imagen,activo) VALUES 
 (1,'Carito','$2a$10$P1.w58XvnaYQUQgZUCk4aO/RTRl8EValluCqB3S2VMLTbRt.tlre.','Carolina', 'Castillo Monge',    'ccastillo@gmail.com',    '8888-8888', 'https://img.freepik.com/free-photo/portrait-beautiful-young-woman-standing-grey-wall_231208-10760.jpg',true),
 (2,'DSolis','$2a$10$GkEj.ZzmQa/aEfDmtLIh3udIH5fMphx/35d0EYeqZL5uzgCJ0lQRi','David',  'Solis Vazques', 'dsolis@gmail.com', '7777-7777','https://img.freepik.com/free-photo/portrait-handsome-young-man-with-crossed-arms_176420-15569.jpg?w=2000',true),
@@ -92,3 +116,9 @@ insert into dbkada.rol (id_rol, nombre, id_usuario) values
  (1,'ROLE_ADMIN',1), (2,'ROLE_VENDEDOR',1), (3,'ROLE_USER',1),
  (4,'ROLE_VENDEDOR',2), (5,'ROLE_USER',2),
  (6,'ROLE_USER',3);
+ 
+ insert into dbkada.tipo (id_tipo, descripcion, activo) values
+ (1,'Planificador',true), (2,'Agenda',true), (3,'Cuaderno',true);
+ 
+ INSERT INTO dbkada.pedido (id_pedido,nombre,pedido,descripcion,telefono,correo_elec,ruta_imagen,activo) VALUES
+(1,'Karen Ruiz',2,'Quiero un libro con la tematica parecida a la foto en caso de que la imagen no de lo suficiente.','8888-8888','kruiz@hotmail.com','https://cdn.discordapp.com/attachments/1132040104370769920/1132040131352723627/2872.png',true);
